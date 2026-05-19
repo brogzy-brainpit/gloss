397d646e8c8ddcad3f7081b2cb3d0e13d67b64c0
unknown <dangabarin2020@gmail.com>
2026-05-19 15:04:02 +0100

    update commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,17 +4,23 @@ import RubberSection from '@/effects/RubberSection';
 import StaggerText from '@/effects/StaggerText';
 import Section from '@/layout/Section';
 import GridColumn from '@/layout/GridColumn';
+import { useMediaQuery } from 'react-responsive';
 
 
 function Services() {
    const ref = useRef(null);  
+      const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
   const texts=
-  [{text:'Individual Coaching',
-      url:'#',img:'/images/service01.png',y:-20,initialY:0
-    }, {text:'Group Coaching',
-      url:'#',img:'/images/service02.png',y:-100,initialY:0
-    }, {text:'Corporate Group Coaching',
-      url:'#',img:'/images/service03.png',y:-180,initialY:0
+  [{text:'ceramic coating',
+    para:'we provide premium car detailing services, restoring your vehicle’s shine inside and out with expert care, deep cleaning, and precision finishing to keep your car looking brand new every time.',
+      url:'#',img:'/images/service01.png',y:isTabletOrMobile ? 0 : -20,initialY:0
+    }, {text:'interior detailing',
+      para:'we provide premium car detailing services, restoring your vehicle’s shine inside and out with expert care, deep cleaning, and precision finishing to keep your car looking brand new every time.',
+      url:'#',img:'/images/service02.png',y:isTabletOrMobile ? 0 : -100,initialY:0
+    }, {text:'exterior detailing',
+      para:'we provide premium car detailing services, restoring your vehicle’s shine inside and out with expert care, deep cleaning, and precision finishing to keep your car looking brand new every time.',
+      url:'#',img:'/images/service03.png',y:isTabletOrMobile ? 0 : -180,initialY:0
     }]
   return (
     <div ref={ref} className='bg-brand-white w-full min-h-[60vh]'>
@@ -41,16 +47,14 @@ function Services() {
               <GridColumn>
                   <div className="grid grid-cols-1 col-span-full md:grid-cols-3 gap-6  w-full">
   {/* Left side - stacked items */}
-  {texts.map(({text,url,img,y,initialY},index)=>{
+  {texts.map(({text,para,url,img,y,initialY},index)=>{
 
      return (
    <RubberSection defaultStart={["start 0.89", "0.7 start"]} defaultY={[initialY, y]} >
-        <div className="cursor-pointer overflow-hidden relative w-full py-10 px-2 h-[350px] rounded-xl items-start justify-end flex gap-4 flex-col">
+        <div className={`${index === 1 ? 'text-brand-black' : 'text-brand-white'} ${index === 1 ? 'bg-brand-secondary' : 'bg-brand-black'} cursor-pointer overflow-hidden flex justify-between  relative w-full py-10 px-2 h-[350px] lg:rounded-xl rounded-sm items-start gap-4 flex-col`}>
   {/* Background image */}
-  <motion.img
-    src={img}
-    className="absolute inset-0 object-cover w-full h-full z-0"
-  />
+ <h2 className=" text-para capitalize  font-body z-10">{text}</h2>
+ <p className=" text-para  z-10">{para}</p>
 
 </div>
    </RubberSection>
